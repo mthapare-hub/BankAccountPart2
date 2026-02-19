d8d5e6b6d1035e226c6efbce0ceae828ad273e06
Marvin Thapa Renteral <mtrenteral@gmail.com>
2026-02-19 00:32:59 -0500

    Fixing code to add protected and private members for the class

--- AccountWithTransferLimitClass.py
+++ AccountWithTransferLimitClass.py
@@ -3,8 +3,8 @@ from BankAccount import MarvinBank
 class AccountWithTransferLimit(MarvinBank):
     bankName = "AccountWithTransferLimit"
 
-    def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit):
-        super().__init__(customer_name, current_balance, minimum_balance)
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number, transfer_limit):
+        super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self.transfer_limit = transfer_limit
     def withdraw(self, balance):
         if balance > self.transfer_limit:
--- Main.py
+++ Main.py
@@ -2,21 +2,21 @@ from BankAccount import *
 from AccountWithTransferLimitClass import *
 from savingsAccount import *
 
-Marvin = MarvinBank("Marvin", 100000, 1000)
+Marvin = MarvinBank("Marvin", 100000, 1000, 123, 987)
 Marvin.deposit(100000)
 Marvin.withdraw(200000)
 Marvin.withdraw(100000)
 
 Marvin.print_customer_info()
 
-Lucy = MarvinBank("Lucy", 200000, 100)
+Lucy = MarvinBank("Lucy", 200000, 100, 456, 765)
 Lucy.deposit(500000)
 Lucy.withdraw(300000)
 # Lucy.withdraw(300000)
 
 Lucy.print_customer_info()
 
-Samuel = AccountWithTransferLimit("Samuel", 100000, 100, 500)
+Samuel = AccountWithTransferLimit("Samuel", 100000, 100, 111, 222, 500)
 Samuel.deposit(100000)
 Samuel.withdraw(100000)
 
