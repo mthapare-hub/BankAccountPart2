e9f5ba95be7c42268ac06113f6bb97aef0a90e19
Marvin Thapa Renteral <mtrenteral@gmail.com>
2026-02-19 00:30:20 -0500

    Adding protected and private members to the class

--- BankAccount.py
+++ BankAccount.py
@@ -1,10 +1,15 @@
 class MarvinBank:
     bankName = "Marvin's Bank"
-    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_numeber):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
+
+        """Protected member (Account Number)"""
         self._account_number = account_number
+
+        """Private member (Routing Number)"""
+        self.__routing_number = routing_number
 
     def deposit(self, balance):
         self.current_balance += balance
