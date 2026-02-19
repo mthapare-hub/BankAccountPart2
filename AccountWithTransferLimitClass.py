d95743133a10aaa66b3bcec6e4791a511ca39452
Marvin Thapa Renteral <mtrenteral@gmail.com>
2026-02-19 00:04:07 -0500

    Implemented class modularity

--- AccountWithTransferLimitClass.py
+++ AccountWithTransferLimitClass.py
@@ -6,12 +6,9 @@ class AccountWithTransferLimit(MarvinBank):
     def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit):
         super().__init__(customer_name, current_balance, minimum_balance)
         self.transfer_limit = transfer_limit
-
     def withdraw(self, balance):
-        if self.current_balance < self.minimum_balance:
-            print("Your current balance is lower than the minimum balance.")
-            return
-        elif balance > self.transfer_limit:
+        if balance > self.transfer_limit:
             print("The amount you're trying to withdraw is higher than the transfer limit. Your transfer limit is: " + str(self.transfer_limit))
+            return
 
-        self.current_balance -= balance
+        super().withdraw(balance)
--- BankAccount.py
+++ BankAccount.py
@@ -21,18 +21,3 @@ class MarvinBank:
         print("Current Balance: " + str(self.current_balance))
         print("Minimum Balance: " + str(self.minimum_balance))
         print("**************************")
-
-
-Marvin =  MarvinBank("Marvin", 100000, 1000)
-Marvin.deposit(100000)
-Marvin.withdraw(200000)
-Marvin.withdraw(100000)
-
-Marvin.print_customer_info()
-
-Lucy = MarvinBank("Lucy", 200000, 100)
-Lucy.deposit(500000)
-Lucy.withdraw(300000)
-#Lucy.withdraw(300000)
-
-Lucy.print_customer_info()
